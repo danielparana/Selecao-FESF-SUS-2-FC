c528242e457f40d451478b244114beba29062d77
daniel <140925788+danielparana@users.noreply.github.com>
2026-06-01 22:40:38 +0000

    Ajuste das rotas de acesso da API

--- frontend/pages/script.js
+++ frontend/pages/script.js
@@ -2,4 +2,8 @@ fetch("http://localhost:5000/api")
   .then(res => res.json())
   .then(data => {
     document.getElementById("msg").innerText = data.message;
+  })
+  .catch(error => {
+    console.error(error);
+    document.getElementById("msg").innerText = "Erro ao conectar com a API";
   });
